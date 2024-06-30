dcd8215f1737278371ed9ebcf8a0931aaf5f174e
Manasseh <rougerapper@gmail.com>
2024-06-30 17:40:21 +0100

    maybe last commit for the month

--- chapter11/main.py
+++ chapter11/main.py
@@ -1,4 +1,33 @@
 import re
+fileHandle = open('mbox-short.txt')
+for line in fileHandle:
+  line = line.rstrip()
+  if re.search("^From:.+@",line):
+    print(line)
+
+
+""" for line in fileHandle:
+  line = line.rstrip()
+  if re.search('^F..m:.+@',line):
+    print(line) """
+
+
+"""if re.search('^From:',line):print(line) """
+""" if re.search('^F..m:',line):
+      print(line) """
+#character matching in regular expressions
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Search for lines that contain 'From'
 """ import re
